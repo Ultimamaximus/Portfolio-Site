effcfd15fb9258794385e8ce844c60cc5649e2cf
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-08-23 14:19:50 -0700

    Initial Commit.

---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'animate.css';
+import './NavBar.css'; 
+
+const NavBar = () => {
+    return (
+      <nav className="navbar animate__animated animate__fadeInDown">
+        <div className="logo-container">
+          <h2>Victor Branson</h2>
+        </div>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <button className="nav-link">About</button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link">Work</button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link">Contact</button>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
+  
