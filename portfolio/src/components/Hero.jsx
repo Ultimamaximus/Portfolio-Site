722af539d4f614fdabf75fa2c2289f52519c1166
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-09-11 10:01:13 -0700

    I added my hero image.

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/Hero.css"
 import 'animate.css';
-import heroImage from '../images/test-img.webp'; 
+import heroImage from '../images/Hero.webp'; 
 
 const Hero = () => {
     return (
